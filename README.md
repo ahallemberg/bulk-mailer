68c3850a6fb888d2df678e6320e0b1c7ab8930a0
ahallemberg <askhb@q-free.com>
2024-09-04 15:55:50 +0200

    Sending wiht the current setup works

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-## Config
+# Bulk Mailer
+Send bulk mails
 
+## Gmail setup 
+1. Go to your Google Account settings.
+2. Select "Security" on the left.
+3. Under "Signing in to Google," select "2-Step Verification" (you need to have this enabled).
+4. At the bottom of the page, select "App passwords."
+5. Choose "Mail" and "Other (Custom name)" from the dropdown menus.
+6. Enter a name for the app (e.g., "Python Email Script") and click "Generate."
+7. Use the 16-character code that appears as your SENDER_PASSWORD in .env (remove any spaces between the characters)
+
+## Config
+Create a .env file and complete these configs
+
+```
 SMTP_SERVER=
+SMTP_PORT=
+SENDER_MAIL=
 SENDER_PASSWORD=
+```
+For gmail:
+```
+SMTP_SERVER=smtp.gmail.com
+SMTP_PORT=587
+```
+
+## Template 
+In mail-conifg.json, the field "template" specify which template to use. Edit the template file to change the template
+
+You can use these special placeholders in your template
+
+- {recipient}
+- {subject}
+- {sender}
--- main.py
+++ main.py
@@ -1,26 +1,60 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+import os
+import csv
+import json
 
-def send_email(recipient, subject, body):
+def load_json_config() -> dict: 
+    file_path = os.path.join(os.path.dirname(__file__), "mail-config.json")
+    with open(file_path, "r", encoding="utf-8") as f: 
+        return json.load(f)
+
+def load_template(file_name: str) -> str: 
+    with open(os.path.join(os.path.dirname(__file__), file_name), encoding="utf-8") as f: 
+        reader = csv.DictReader(f)
+
+        return f.read()
+
+def send_email(smtp_server: str, smtp_port: int, sender: str, sender_password: str, recipient: str, subject: str, body: str) -> None:
+    full_name = recipient.split("@")[0].split("")
+    # special replacements for template
+    body = body.replace('{recipient}', recipient)
+    body = body.replace('{subject}', subject)
+    body = body.replace('{sender}', sender)
+    body = body.replace('{full_name}', full_name)
+
+    # create message
     message = MIMEMultipart()
-    message["From"] = SENDER_EMAIL
+    message["From"] = sender
     message["To"] = recipient
     message["Subject"] = subject
     message.attach(MIMEText(body, "plain"))
 
-    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
         server.starttls()
-        server.login(SENDER_EMAIL, SENDER_PASSWORD)
+        server.login(sender, sender_password)
         server.send_message(message)
 
-def main():
-    for email in RECIPIENTS:
+def main() -> None:
+    load_dotenv()
+    SMTP_SERVER = os.getenv("SMTP_SERVER")
+    SMTP_PORT = os.getenv("SMTP_PORT")
+    SENDER_MAIL = os.getenv("SENDER_MAIL")
+    SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
+    _cfg = load_json_config()
+    RECIPIENTS = _cfg["recipients"]
+    SUBJECT = _cfg["subject"]
+    TEMPLATE = load_template(_cfg["template"])
+
+    for recipient in RECIPIENTS:
         try:
-            send_email(email, EMAIL_SUBJECT, EMAIL_TEMPLATE)
-            print(f"Email sent successfully to {email}")
+            send_email(SMTP_SERVER, SMTP_PORT, SENDER_MAIL, SENDER_PASSWORD, recipient, SUBJECT, TEMPLATE)
+            print(f"Email sent successfully to {recipient}")
         except Exception as e:
-            print(f"Failed to send email to {email}: {str(e)}")
+            print(f"Failed to send email to {recipient}: {str(e)}")
+            raise e
 
 if __name__ == "__main__":
     main()
